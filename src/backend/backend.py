d3c0a6f251dd60796a41e8a339fafd226e3340e7
Dmitry Demitov <demitov@gmail.com>
2022-04-15 02:40:59 +0400

    backend: make AWS Lambda function. Get data from API, write to DynamoDB table

--- src/backend/backend.py
+++ src/backend/backend.py
@@ -1,36 +1,21 @@
 import json
 import requests
 from datetime import datetime
-import sqlalchemy as sa
-from sqlalchemy.orm import Session, relationship
-from sqlalchemy.ext.declarative import declarative_base
-# import os
-
+import boto3
+#import os
 
 # CLIENT_ID = os.environ['CLIENT_ID']
-client_id = 'fc172899-0d19-40dc-9a1f-88949b778e48'
-
+CLIENT_ID = 'fc172899-0d19-40dc-9a1f-88949b778e48'
 RESULT_DATA = list()
 
-Base = declarative_base()
-engine = sa.create_engine('sqlite:///stations.db', echo=False)
+client = boto3.client('dynamodb')
+dynamodb = boto3.resource('dynamodb')
 
+table_name = 'demitov-v13'
 
-class Stations(Base):
-    __tablename__ = 'stations'
-    id        = sa.Column(sa.Integer, primary_key=True) #, autoincrement=True)
-    sourceId  = sa.Column(sa.String(12), unique=False, nullable=False)
-    name      = sa.Column(sa.String(32), unique=False, nullable=False)
-    validFrom = sa.Column(sa.DateTime, unique=False, nullable=False)
-
-    def __init__(self, sourceId, name, validFrom):
-        self.sourceId  = sourceId
-        self.name      = name
-        self.validFrom = validFrom
-
-
+# Get data from API
 def get_data(endpoint: str, parameters: dict):
-    r = requests.get(endpoint, parameters, auth=(client_id, ''))
+    r = requests.get(endpoint, parameters, auth=(CLIENT_ID, ''))
     json = r.json()
 
     if r.status_code == 200:
@@ -41,7 +26,7 @@ def get_data(endpoint: str, parameters: dict):
         print('Reason: %s' % json['error']['reason'])
     return data
 
-
+# Get stations Norway
 def get_stations():
     endpoint = 'https://frost.met.no/sources/v0.jsonld'
     parameters = {
@@ -54,12 +39,13 @@ def get_stations():
 
     for record in data:
         sensor_data = dict(
-            sourceId = record['id'],
-            name     = record['name'],
+            sourceId   = record['id'],
+            sourceName = record['name'],
         )
         RESULT_DATA.append(sensor_data)
 
 
+# Get avaliable times for stations
 def get_avaliable_times():
     endpoint = 'https://frost.met.no/observations/availableTimeSeries/v0.jsonld'
     parameters = {
@@ -72,37 +58,49 @@ def get_avaliable_times():
     for record in data:
         for row in RESULT_DATA:
                 if row['sourceId']== record['sourceId'][:-2]:
-                    row['validFrom'] = datetime.fromisoformat(record['validFrom'][:-1])
+                    row['validFrom'] = int(round(datetime.fromisoformat(record['validFrom'][:-1]).timestamp()))
+
+# Init table in DynamoDB
+def init_table():
+    existing_tables = client.list_tables()['TableNames']
+
+    if table_name not in existing_tables:
+        table = client.create_table(
+            TableName=table_name,
+            KeySchema=[{'AttributeName': 'sourceId', 'KeyType': 'HASH'}],
+            AttributeDefinitions=[{'AttributeName': 'sourceId', 'AttributeType': 'S'}],
+            ProvisionedThroughput={'ReadCapacityUnits':10, 'WriteCapacityUnits':10}
+        )
+
+# Update data in table
+def update_table():
+    table = dynamodb.Table(table_name)
+    for item in RESULT_DATA:
+        try:
+            table.update_item(
+                Key={'sourceId':item['sourceId']},
+                UpdateExpression="set #n=:sourceName, #v=:validFrom",
+                ExpressionAttributeNames={
+                    '#n': 'sourceName',
+                    '#v': 'validFrom'
+                },
+                ExpressionAttributeValues={
+                    ':sourceName': item['sourceName'],
+                    ':validFrom': item['validFrom']
+                },
+            )
+        except Exception as e:
+            print (e)
 
 
-def db_init():
-    Base.metadata.create_all(engine)
+def lambda_handler(event, context):
+    init_table()
+    get_stations()
+    get_avaliable_times()
+    update_table()
 
-
-def db_write():
-
-    with Session(engine) as session:
-        for item in RESULT_DATA:
-            sourceId = item['sourceId']
-            name = item['name']
-            if item.get('validFrom'):
-                validFrom = item['validFrom']
-
-            entry = Stations(sourceId, name, validFrom)
-            session.add(entry)
-
-        session.commit()
-
-
-# MOVE TO LAMBDA FUNCTION
-db_init()
-get_stations()
-get_avaliable_times()
-db_write()
-
-# -----------------------------------------------------------------------------
-# def lambda_handler(event, context):
-#     return {
-#         'statusCode': 200)
-#     }
-# -----------------------------------------------------------------------------
+    message = 'From API Gateway {} {} {}!'.format(event['type'], event['authorizationToken'], event['methodArn'])
+    return {
+        'statusCode': 200,
+        'message' : message
+    }
